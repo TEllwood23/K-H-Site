1f71b4cdff79cfcc2f057653d66da029e036d299
Thomas Ellwood <taellwood@gmail.com>
2024-11-22 14:03:54 +0000

    Added routing for Home and About pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+// src/pages/About.jsx
+
+function About() {
+  return (
+    <div>
+      <h1>This is the About</h1>
+      <p>Lots of info about Kieran.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+// src/pages/Home.jsx
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Website- with new home page</h1>
+      <p>This is the main landing page for the site.</p>
+
+      {/* Move Vite logo, React logo, and counter here */}
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src="/react.svg" className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => alert('Count functionality can be added!')}>
+          This is just a demo button
+        </button>
+        <p>
+          Edit <code>src/pages/Home.jsx</code> to customize this page.
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default Home;
