7a70099741293125400a1580ec75ed0939975670
Thomas Ellwood <taellwood@gmail.com>
2024-11-24 13:31:18 +0000

    fixed isse with margin on Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@
 
 function Home() {
   return (
-    <div className="mt--100">
-      <h1 className="text-textPrimary font-sans">Welcome to My Website- with new home page</h1>
+    <div>
+      <h1 className="text-textPrimary font-sans">Welcome to My Website - with new home page</h1>
       <p className="text-textSecondary font-sans">This is the main landing page for the site.</p>
     </div>
   );
