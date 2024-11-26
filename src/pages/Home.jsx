be8e00379abafeada1289cf852e1dd575118bf0c
Thomas Ellwood <taellwood@gmail.com>
2024-11-26 17:31:00 +0000

    added image to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,12 @@ function Home() {
   return (
     <div>
       <h1 className="text-textPrimary text-6xl font-sans">Kieran Hodgson</h1>
-      <p className="text-textSecondary font-sans">This is the main landing page for the site.</p>
+      <p className="text-textPrimary font-sans">Actor - Comedian- Writer.</p>
+        <img
+          src="/images/KH_Big_Scotland_2_with_quote.png"
+          alt="Kieran Hodgson performing his Big in Scotland show"
+          className="w-2/3 sticky left-0 object-cover rounded-lg shadow-lg"
+        />
     </div>
   );
 }
