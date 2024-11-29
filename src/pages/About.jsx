ddd1cb2de04e7ac6853c3acfce5902680ff764e2
Thomas Ellwood <taellwood@gmail.com>
2024-11-29 16:46:03 +0000

    further editing of responsiveness

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,8 @@
 function About() {
   return (
-    <div className="container mx-auto px-8 h-screen pt-20 lg:pt-40">
+    <div className="container mx-auto px-8 h-screen pt-36 lg:pt-40">
       <h1 className="text-4xl pb-4 font-sub">Bio...</h1>
-      <div className="flex flex-col lg:flex-row text-sm md:text-xl h-48 text-wrap">
+      <div className="flex flex-col lg:flex-row text-sm md:text-2xl lg:text-3xl h-48 text-wrap">
         <p className="flex-1">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel nulla a libero egestas commodo. Phasellus eu turpis et massa tincidunt sodales a non erat. Aliquam pretium sapien at lacinia egestas. Pellentesque bibendum ultricies auctor. Morbi facilisis tempus mauris eget efficitur. Vivamus id ex id odio efficitur venenatis.
         </p>
@@ -10,7 +10,7 @@ function About() {
           <img
             src="/images/Kieran_headshot.jpeg"
             alt="Kieran Hodgson"
-            className="rounded-lg flex-grow max-h-[50vh] h-auto w-auto object-contain mx-auto mt-2"
+            className="rounded-lg flex-grow max-h-[50vh] h-auto w-auto object-contain mx-auto mt-2 md:mt-6 lg:mt-0"
           />
         </div>
       </div>
